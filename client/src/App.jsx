d1a7688f57dc7b9a9df201ba87702875676f0c35
Nguyen Vo Minh Tri <minhtri.nguyenvo31@gmail.com>
2023-05-29 13:22:53 +0700

    implement team channel list and team channel preview component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,17 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import {ChannelListContainer , ChannelContainer} from './components'
+import {ChannelListContainer , ChannelContainer, Auth} from './components'
 
 const apiKey = '1251656';
 
-const client = StreamChat.getInstance(apiKey)
+const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
+
 
 function App() {
+  if(!authToken) return <Auth/>
   return (
     <div className="app__wrapper">
       <Chat client={client} theme = "team light">
